3f3055b8fd50e235b2276f422aa513afea081bce
zyntax-zx <68755927+zyntax-zx@users.noreply.github.com>
2026-04-21 15:19:42 -0500

    Update visual_feedback.mm

--- visual_feedback/visual_feedback.mm
+++ visual_feedback/visual_feedback.mm
@@ -2,20 +2,17 @@
 #import <UIKit/UIKit.h>
 #include "../utils/utils.h"
 
-void show_load_toast() {
+extern "C" void show_load_toast() {
     dispatch_async(dispatch_get_main_queue(), ^{
         UIAlertController *alert = [UIAlertController 
             alertControllerWithTitle:@"✅ enhanced-ios-hacker"
-            message:@"Dylib cargada correctamente\niOS 26 jailed - ESign mode"
+            message:@"Dylib cargada correctamente\niOS 26 - ESign mode"
             preferredStyle:UIAlertControllerStyleAlert];
         
         [alert addAction:[UIAlertAction actionWithTitle:@"OK" 
-            style:UIAlertActionStyleDefault 
-            handler:nil]];
+            style:UIAlertActionStyleDefault handler:nil]];
         
         [[UIApplication sharedApplication].keyWindow.rootViewController 
             presentViewController:alert animated:YES completion:nil];
-        
-        utils::log_to_file("✅ Toast visual mostrado - dylib cargada");
     });
 }
