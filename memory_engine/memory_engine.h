927cdc273f61a3a5dfb095037477405f6b7e87b2
zyntax-zx <68755927+zyntax-zx@users.noreply.github.com>
2026-04-21 11:09:47 -0500

    Update memory_engine.h

--- memory_engine/memory_engine.h
+++ memory_engine/memory_engine.h
@@ -1,4 +1,7 @@
+// memory_engine/memory_engine.h
 #pragma once
+#include <cstdint>     // ← Esta línea faltaba
+
 namespace memory_engine {
     void init();
     bool write_float(uint64_t addr, float value);
